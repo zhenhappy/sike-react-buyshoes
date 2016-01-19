29f62188d086dc1be4cb8b42daa6cbed14a20056
zhenhappy <q505507538@gmail.com>
2016-01-19 13:17:51 +0800

    使用 Perfect Scrolling

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 window.onload = function() {
   console.log("page loaded");
+  makeCartScrollNicely();
+}
+
+function makeCartScrollNicely() {
+  var cart = document.querySelector('.cart__content');
+  Ps.initialize(cart);
 }
